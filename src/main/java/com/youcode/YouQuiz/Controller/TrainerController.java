5472616d6aa3aa5292a8809f0198bcf72ff043c4
Alikhalif <abdelalikhalif1999@gmail.com>
2023-11-07 22:38:59 +0100

    SCRUM-7 Get a trainer by id

--- src/main/java/com/youcode/YouQuiz/Controller/TrainerController.java
+++ src/main/java/com/youcode/YouQuiz/Controller/TrainerController.java
@@ -4,6 +4,8 @@ import com.youcode.YouQuiz.Service.ImplService.TrainerServiceImpl;
 import com.youcode.YouQuiz.dto.TrainerDto;
 import com.youcode.YouQuiz.entities.Trainer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +25,16 @@ public class TrainerController {
     @GetMapping
     public List<Trainer> getAllTrainer(){
         return trainerService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TrainerDto> getTrainer(@PathVariable Long id){
+        TrainerDto trainerDto = trainerService.getOne(id);
+        if(trainerDto != null){
+            return new ResponseEntity<>(trainerDto, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/{id}")
--- src/main/java/com/youcode/YouQuiz/Service/ImplService/TrainerServiceImpl.java
+++ src/main/java/com/youcode/YouQuiz/Service/ImplService/TrainerServiceImpl.java
@@ -4,17 +4,22 @@ import com.youcode.YouQuiz.Service.TrainerService;
 import com.youcode.YouQuiz.dto.TrainerDto;
 import com.youcode.YouQuiz.entities.Trainer;
 import com.youcode.YouQuiz.repositories.TrainerRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TrainerServiceImpl implements TrainerService {
 
     @Autowired
     private TrainerRepository trainerRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     public Trainer create(TrainerDto trainerDto){
         Trainer trainer = new Trainer();
@@ -28,6 +33,12 @@ public class TrainerServiceImpl implements TrainerService {
 
     public List<Trainer> getAll(){
         return trainerRepository.findAll();
+    }
+
+
+    public TrainerDto getOne(Long id){
+        Optional<Trainer> trainer = trainerRepository.findById(id);
+        return trainer.map(trainer1 -> modelMapper.map(trainer1, TrainerDto.class)).orElse(null);
     }
 
     public Trainer update(Long id, TrainerDto trainerDto){
