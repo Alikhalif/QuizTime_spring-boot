7df0761256c2e60ebb0f5edd88367660559c51b2
Alikhalif <abdelalikhalif1999@gmail.com>
2023-11-20 11:21:19 +0100

    SCRUM-104 save student answar

---
+++ src/main/java/com/youcode/YouQuiz/Controller/AnswarStudentController.java
@@ -0,0 +1,2 @@
+package com.youcode.YouQuiz.Controller;public class AnswarStudentController {
+}
---
+++ src/main/java/com/youcode/YouQuiz/dto/StudentAnswar/StudentAnswarResponsDto.java
@@ -0,0 +1,2 @@
+package com.youcode.YouQuiz.dto.StudentAnswar;public class StudentAnswarResponsDto {
+}
